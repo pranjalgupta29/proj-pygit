8b11cf9269cc98378a77254f40fa1299cc3731e6
Pranjal <pgupta3_be19@thapar.edu>
2021-09-19 22:41:29 +0530

    Object Read

--- libwyag.py
+++ libwyag.py
@@ -185,3 +185,33 @@ class GitObject(object):
 
     def deserialize(self, data):
         raise Exception("Unimplemented!")
+
+def object_read(repo, sha):
+    """Read object object_di from Git repository repo. Return a GitObject whose
+    exact type depends on the object itself."""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception("Malformed object {0}:  bad length".format(sha))
+
+        # Pick Constructor
+        if fmt==b'commit'   :   c=GitCommit
+        elif fmt==b'tree'   :   c=GitTree
+        elif fmt==b'tag'   :   c=GitTag
+        elif fmt==b'blob'   :   c=GitBlob
+        else:
+            raise Exception("Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha))
+
+        # Call constructor and return object
+        return c(repo, raw[y+1:])
